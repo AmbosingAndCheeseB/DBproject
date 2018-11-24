ae18f05ae1ca4aa6c1ad986296647e8dbef50ed3
yangjiwon <ambosing_@naver.com>
2018-11-24 21:48:14 +0900

    지도 api

--- main.php
+++ main.php
@@ -87,6 +87,18 @@ session_start();
     </table>
   </form>
 </div>
+	
+		<div id="map" style="width:500px;height:400px;"></div>
+	<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=f722b2f37d3075fced8b4fa988359be7"></script>
+	<script>
+		var container = document.getElementById('map');
+		var options = {
+			center: new daum.maps.LatLng(33.450701, 126.570667),
+			level: 3
+		};
+
+		var map = new daum.maps.Map(container, options);
+	</script>
 
 </body>
 </html>
