28b016e54c4fdd1978f3857aded4eb4831ba736c
yangjiwon <ambosing_@naver.com>
2018-12-15 18:45:20 +0900

    지도 api 수정

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php   
 include "code/dbconfig.php";
 session_start();
+	
+	$sql = 'select * from hospital';
+	$result = $db->query($sql);
+
 ?>
 
 <!DOCTYPE html>
@@ -87,15 +91,64 @@ session_start();
 	
 	
 <div id="map" style="margin:40px auto 150px auto; width:1000px;height:600px;"></div>
-	<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=f722b2f37d3075fced8b4fa988359be7"></script>
+	<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=f722b2f37d3075fced8b4fa988359be7&libraries=services"></script>
 	<script>
 		var container = document.getElementById('map');
 		var options = {
-			center: new daum.maps.LatLng(33.450701, 126.570667),
-			level: 3
+			center: new daum.maps.LatLng(35.8343547, 127.1292019),
+			level: 8
 		};
 
 		var map = new daum.maps.Map(container, options);
+		// 주소-좌표 변환 객체를 생성합니다
+		var geocoder = new daum.maps.services.Geocoder();
+		
+		<?php
+			while($row = $result->fetch_array())
+			{
+				
+		?>
+		geocoder.addressSearch('<?php echo $row['Address']; ?>', function(result, status) {
+
+		// 정상적으로 검색이 완료됐으면 
+			 if (status === daum.maps.services.Status.OK) {
+
+				var coords = new daum.maps.LatLng(result[0].y, result[0].x);
+
+				// 결과값으로 받은 위치를 마커로 표시합니다
+				var marker = new daum.maps.Marker({
+					map: map,
+					position: coords
+				});
+
+				// 인포윈도우로 장소에 대한 설명을 표시합니다
+				var infowindow = new daum.maps.InfoWindow({
+					content: '<div style="width:150px;text-align:center;padding:6px 0;"><?php echo $row['Hospital_Name']."<br>".$row['Call_Number']?></div>'
+				});
+				//infowindow.open(map, marker);
+				 
+				 daum.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
+				 daum.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
+				
+				 function makeOverListener(map, marker, infowindow) {
+				return function() {
+					infowindow.open(map, marker);
+				};
+			}
+
+			// 인포윈도우를 닫는 클로저를 만드는 함수입니다 
+			function makeOutListener(infowindow) {
+				return function() {
+					infowindow.close();
+				};
+			}
+			
+			}
+			
+
+		});
+		
+		<?php }?>
 	</script>
 </div>
 
