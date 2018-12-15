70fc56970989f74f419f4809e15b8fa2ded43f8e
CheeseBall <2489ckckck@naver.com>
2018-12-15 22:33:55 +0900

    modify

--- code/login.php
+++ code/login.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<link rel= "stylesheet" href="../css/login.css">
+	<link rel= "stylesheet" href="../css/menubar.css">
 	<style>
 		body {
 			background:#DBF0F8;
@@ -12,13 +13,19 @@
 
 	
 <body>
+	
+	<ul class="menubar">
+  		<li><a href="../index.php">홈</a></li>
+ 	 	<li><a href="../code/board.php">게시판</a></li>
+	</ul>
+	
 	<section>
 	<form method="post" action="login_ok.php">
 				
 		 		<h1>회원 로그인</h1>
                 <input type="text" name="userid" placeholder="ID" required>
 				
-               	<input type="password" name="userpw" placeholder="Password" required>
+               	<input type="password" name="userpw" placeholder="Password" style="margin-bottom: 70px" required>
 				
         		<a href="signup.php">계정이 없으신가요?</a> </label>
 				
