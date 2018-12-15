0063b6e0ae227221954f7670854992e61eae64fb
CheeseBall <2489ckckck@naver.com>
2018-12-15 21:43:25 +0900

    modify

--- index.php
+++ index.php
@@ -23,26 +23,6 @@ session_start();
 
 <body>
 
-	<div id="login_box" class="wrapper">
-	
-	<?php
-	
-	if(!$_SESSION['is_login']){
-		echo '	<html><body>
-				<form method="post" action="code/login_ok.php" class="form-signin">
-				
-		 		<h2 class="form-signin-heading">Please login</h2>
-                <input type="text" name="userid" class="form-control" placeholder="ID" required>
-				
-               	<input type="password" name="userpw" class="form-control" placeholder="Password" required>
-				
-				<label class="checkbox">
-        		<a href="code/signup.php">회원가입 하시겠습니까?</a> </label>
-				
-				<button type="submit" id="btn" class="btn btn-lg btn-primary btn-block" >로그인</button>
-				
-				</form> </body></html>'; }
-?>
 	
 <ul class="menubar">
   <li><a href="index.php">홈</a></li>
